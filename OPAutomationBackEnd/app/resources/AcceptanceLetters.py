eed6c8d916f7664d8d2c39b2f554accd73a96650
saishanmukh591 <89558232+saishanmukh591@users.noreply.github.com>
2022-06-24 10:25:47 -0500

    acceptance_letters endpoint added

--- OPAutomationBackEnd/app/resources/AcceptanceLetters.py
+++ OPAutomationBackEnd/app/resources/AcceptanceLetters.py
@@ -1,8 +1,25 @@
-from flask import Blueprint, abort, jsonify
-import requests
-
-import requests
-from apifairy import authenticate, body, response, other_responses, arguments
-
+from flask import Blueprint, request, send_file
+# from apifairy import authenticate, body, response, other_responses, arguments
+import time
+import os
+import zipfile
+import io
+from app.resources.utils import AcceptanceLetters
 
 acceptance_letters = Blueprint('acceptance_letters', __name__)
+
+@acceptance_letters.route("/acceptance_letters", methods= ['POST', 'GET'])
+def generate_acceptance_letters():
+    temp_folder_name = os.path.join(os.getenv("TEMP_FOLDER"), str(time.time_ns()))
+    os.mkdir(temp_folder_name)
+    data_file = request.files["file"]
+    if AcceptanceLetters.generate_acceptance_letters(data_file, temp_folder_name):
+
+        memory_file = io.BytesIO()
+        with zipfile.ZipFile(memory_file, 'w') as zf:
+            files = os.listdir(temp_folder_name)
+            for filename in files:
+                FILEPATH = os.path.join(temp_folder_name, filename)
+                zf.write(FILEPATH, arcname=filename)
+        memory_file.seek(0)
+        return send_file(memory_file, mimetype='application/zip', attachment_filename="download.zip")
